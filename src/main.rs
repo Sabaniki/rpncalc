501a3602921bafd844f7a6e2ab2ad02dc713db7f
Sabaniki <sawada13@mccc.jp>
2021-02-07 01:47:05 +0900

    マクロを使用した場合

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,28 @@
-use clap::{App, Arg};
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "mini RPN calc",
+    version = "1.0.0",
+    author = "sabaniki",
+    about = "This is mini RPN(Reverse Polish Notation) program"
+)]
+
+struct Opts {
+    // Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    // Formulas written in PRN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    let matches = App::new("mini RPN calc")
-        .version("1.0.0")
-        .author("sabaniki")
-        .about("This is mini RPN(Reverse Polish Notation) calculator program")
-        .arg(
-            Arg::with_name("formula_file")
-                .help("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("verbose")
-                .help("Sets the level of verbosity")
-                .short("v")
-                .long("verbose")
-                .required(false),
-        )
-        .get_matches();
-
-    match matches.value_of("formula_file") {
+    let opts = Opts::parse();
+    match opts.formula_file {
         Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified.")
+        None => println!("No file specified."),
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+    println!("is verbosity specified?: {}", opts.verbose);
 }
